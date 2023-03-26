6a008edb856b2ef58801284b06915bf2c213a6f6
echcherrate.ismail <echcherrate.ismail@gmail.com>
2023-03-26 23:17:36 +0200

    Query, Paging and Sorting with Spring Data JPA

--- src/main/java/guru/springframework/jdbc/dao/BookDaoHibernate.java
+++ src/main/java/guru/springframework/jdbc/dao/BookDaoHibernate.java
@@ -7,6 +7,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
+
 import java.util.List;
 
 /**
@@ -21,7 +22,19 @@ public class BookDaoHibernate implements BookDao {
 
     @Override
     public List<Book> findAllBooksSortByTitle(Pageable pageable) {
-        return null;
+        EntityManager em = getEntityManager();
+
+        try {
+            String hql = "SELECT b FROM Book b order by b.title " + pageable.getSort()
+                    .getOrderFor("title").getDirection().name();
+
+            TypedQuery<Book> query = em.createQuery(hql, Book.class);
+            query.setFirstResult(Math.toIntExact(pageable.getOffset()));
+            query.setMaxResults(pageable.getPageSize());
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
     }
 
     @Override
@@ -75,7 +88,7 @@ public class BookDaoHibernate implements BookDao {
 
             return (Book) query.getSingleResult();
         } finally {
-            em.close();;
+            em.close();
         }
     }
 
@@ -113,7 +126,7 @@ public class BookDaoHibernate implements BookDao {
         em.close();
     }
 
-    private EntityManager getEntityManager(){
+    private EntityManager getEntityManager() {
         return emf.createEntityManager();
     }
 }
